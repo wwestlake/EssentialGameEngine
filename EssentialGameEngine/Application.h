f26eee04d044d9a895f2e17a301e335de7116cd9
wwestlake <wwestlake@lagdaemon.com>
2015-03-09 20:10:01 -0400

    Addedd GNU GPL License to source files

--- EssentialGameEngine/Application.h
+++ EssentialGameEngine/Application.h
@@ -1,3 +1,27 @@
+/*
+* This file is part of Essential Game Engine.
+*
+* Essential Game Engine is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Essential Game Engine is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+*
+* wwestlake@laqgdaemon.com
+*
+* change notes:
+---------------------------------------------------------------
+
+
+
+*/
 #pragma once
 
 #include "Macros.h"
